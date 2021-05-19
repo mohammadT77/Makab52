566268afaf329d5bd03c3d13459c53e46cb2b3c6
mohammadT77 <t.mohammadamin@gmail.com>
2021-05-19 15:27:25 +0430

    Session 22-23: regex_time_extract.py, regex_time_extract_dict.py - init

---
+++ session22-23/regex_time_extract_dict.py
@@ -0,0 +1,20 @@
+import re
+
+txt = """Non tempora amet 1994-02-24 18:26:25.680292 est. Sed dolor labore ut labore velit porro tempora. Quisquam 
+dolor non voluptatem. Numquam quiquia adipisci dolore eius numquam amet voluptatem. Adipisci 2010-09-15 
+14:39:40.982917 non sed est quiquia dolore quisquam est. Ut tempora quisquam amet  1998-03-16 16:14:16.647591 
+quisquam amet numquam.  2006-02-04 09:14:57.833855 Aliquam dolor quisquam neque eius dolorem. Dolore velit amet 
+quaerat consectetur dolor velit. Sed adipisci 2017-05-01 00:25:34.398268 eius dolorem magnam. Etincidunt adipisci 
+2000-09-17 15:29:24.527475 sit numquam dolor velit d 2013-01-11 21:58:55.007595 olorem eius.\n\nSit numquam dolor 
+velit neque. 2010-11-14 05:14:47.704542 Sed etincidunt numquam 1994-01-14 03:42:59.505113 neque non sit velit. 
+Quiquia numquam adipisci adipisci sed est etincidunt. """
+
+time_pattern = r"(?P<time>(?P<h>\d{2}):(?P<m>\d{2}):(?P<s>\d{2})(?P<ns>.\d+)?)"
+for time_match in re.finditer(time_pattern, txt):
+    print()
+    print('Time:', time_match)
+    print('Groups:', time_match.groupdict())
+    print('Hour:', time_match.group('h'))
+    print('Minute:', time_match.group('m'))
+    print('Second:', time_match.group('s'))
+    print('Nano_secs:', time_match.group('ns'))
